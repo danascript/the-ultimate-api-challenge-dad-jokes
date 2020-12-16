c7a263ab0631720240e5029af0775e847c89021f
Daniela <daniela.fedyakin@gmail.com>
2020-12-16 19:38:48 +0100

    add starter-files

---
+++ starter-files/script.js
@@ -0,0 +1,11 @@
+/**
+ * 1. Initialize an XMLHttpRequest constructor
+ * 2. Open a GET request, set the headers and response type
+ * 3. Output successful response
+ * 4. Output error state
+ * 5. Combine with an event listener (button)
+ * 6. Adjust UI states accordingly
+ * 7. Bonus: change button CTA to indicate if it's the first joke or a "next" one
+ */
+
+const API_ENDPOINT = 'https://icanhazdadjoke.com/';
